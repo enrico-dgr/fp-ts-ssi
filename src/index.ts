ac523510547ffdcc981532b9c0a2d99a85af53d5
enrico-dgr <enrico.digrazia98@gmail.com>
2023-06-24 12:13:29 +0200

    published npm package

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,35 @@
+import { pipe } from 'fp-ts/function'
+import * as A from 'fp-ts/Array'
+import * as E from 'fp-ts/Either'
 import { BuildDeps } from './doOnPattern'
 import { compileFunctions } from './functions'
+import { FileInfo, readFilesSync } from '@enrico-dgr/fp-ts-fs'
 
 type Deps = BuildDeps<{
   filePath: string
   params: Record<string, string | undefined>
 }>
 
-export { compileFunctions as compile, Deps }
+const compileHtmlFiles = (
+  deps: Parameters<typeof readFilesSync>[0] & Pick<Deps, 'params'>
+): E.Either<Error, FileInfo[]> =>
+  pipe(
+    readFilesSync(deps),
+    E.map(
+      A.map((f) =>
+        pipe(
+          compileFunctions({
+            params: { ...deps.params },
+            filePath: f.path,
+            content: f.content,
+          }),
+          (c) => ({
+            content: c,
+            path: f.path,
+          })
+        )
+      )
+    )
+  )
+
+export { compileFunctions as compile, compileHtmlFiles, Deps }
